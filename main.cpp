724a70713de97c9c0388edb5638ceeacfe55761b
thanh.nguyen <thanh.nguyen@steinsvik.vn>
2017-08-01 22:31:13 +0700

    Prototype auto resize

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-
+//QPushButton
+//{background-color: white;
+// border-style: solid;
+// border-width:1px;
+// border-radius:20px;
+// border-color: red;
+//}
 
 int main(int argc, char *argv[])
 {
